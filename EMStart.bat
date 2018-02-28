63fa20964b34d53d8ba3e33a1415fb0323659178
Max <liyue4158@gmail.com>
2018-02-28 10:12:33 -0500

    update script

--- EMStart.bat
+++ EMStart.bat
@@ -1,3 +1,5 @@
 %ECHO OFF
+javac *.java
+
 START "EVENT MANAGER REGISTRY" /MIN /NORMAL rmiregistry
-START "EVENT MANAGER" /MIN /NORMAL java MessageManager
+START "EVENT MANAGER" /MIN /NORMAL java -Djava.rmi.server.hostname="%1" MessageManager %1
